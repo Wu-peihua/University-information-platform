f88250958bc4d7f1ffffbf4a8ee9af262342831c
Wu-peihua <13128684834@163.com>
2020-03-22 18:54:29 +0800

    学生端组队招聘下拉筛选框UI实现

--- FrontEnd/app/src/main/java/com/example/uipfrontend/Student/Fragment/StudentGroupFragment.java
+++ FrontEnd/app/src/main/java/com/example/uipfrontend/Student/Fragment/StudentGroupFragment.java
@@ -10,15 +10,68 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.uipfrontend.R;
+import com.qlh.dropdownmenu.DropDownMenu;
+import com.qlh.dropdownmenu.view.MultiMenusView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class StudentGroupFragment extends Fragment {
+
+    private String[] headers;//菜单头部选项
+    private List<View> popupViews = new ArrayList<>();//菜单列表视图
+    private DropDownMenu dropDownMenu;
+    private MultiMenusView multiMenusView;//多级菜单
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_student_group,null);
+        dropDownMenu = view.findViewById(R.id.dropDownMenu_student_group);
+
+        init();
+
         return view;
+    }
+
+    public void init() {
+        initMenus();
+        initListener();
+    }
+
+    private void initMenus() {
+
+        headers = new String[]{"所属院校"};
+        //初始化多级菜单
+        final String[] levelOneMenu = {"全部", "华南师范大学", "华南理工大学", "中山大学"};
+        final String[][] levelTwoMenu = {
+                {"哲学", "经济学", "法学", "教育学", "文学", "历史学", "理学", "工学", "农学", "医学", "军事学", "管理学","艺术学"},
+                {"哲学", "经济学", "法学", "教育学", "文学", "历史学", "理学", "工学", "农学", "医学", "军事学", "管理学","艺术学"},
+                {"哲学", "经济学", "法学", "教育学", "文学", "历史学", "理学", "工学", "农学", "医学", "军事学", "管理学","艺术学"},
+                {"哲学", "经济学", "法学", "教育学", "文学", "历史学", "理学", "工学", "农学", "医学", "军事学", "管理学","艺术学"}
+        };
+        multiMenusView = new MultiMenusView(this.getContext(),levelOneMenu,levelTwoMenu);
+        popupViews.add(multiMenusView);
+        //初始化内容视图
+        View contentView = LayoutInflater.from(this.getContext()).inflate(R.layout.fragment_student_group,null);
+        //装载
+        dropDownMenu.setDropDownMenu(Arrays.asList(headers),popupViews,contentView);
+
+    }
+
+    private void initListener() {
+
+        //下拉菜单
+        multiMenusView.setOnSelectListener(new MultiMenusView.OnSelectListener() {
+            @Override
+            public void getValue(String showText) {
+                dropDownMenu.setTabText(showText);
+                dropDownMenu.closeMenu();
+            }
+        });
+
     }
 }
