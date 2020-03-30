9e082d40f5e78ce64360c0ec7f0253aa9f3f6866
iamzzh <49836084+iamzzh@users.noreply.github.com>
2020-03-30 17:40:29 +0800

    Create CourseAdapter.java

---
+++ FrontEnd/app/src/main/java/com/example/uipfrontend/Admin/Adapter/CourseAdapter.java
@@ -0,0 +1,48 @@
+package com.example.uipfrontend.Admin.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.uipfrontend.Entity.Course;
+import com.example.uipfrontend.R;
+
+import java.util.List;
+
+public class CourseAdapter extends ArrayAdapter<Course> {
+    private int resourceId;
+
+
+
+    public CourseAdapter(Context context, int textViewResourceId, List<Course> fruits){
+        super(context, textViewResourceId, fruits);
+        this.resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        Course course = getItem(position);
+        View view = LayoutInflater.from(this.getContext()).inflate(resourceId, null);
+
+        //ImageView courseImage = (ImageView)view.findViewById(R.id.courseImage);
+
+
+        TextView courseName = (TextView)view.findViewById(R.id.admin_coursename);
+        TextView teacher = (TextView)view.findViewById(R.id.admin_Teacher);
+        TextView description = (TextView)view.findViewById(R.id.admin_courseDescription);
+        TextView score = (TextView)view.findViewById(R.id.admin_RatingScore);
+
+
+        //courseImage.setImageResource(course.getImageurl());
+        courseName.setText(course.getName());
+        teacher.setText(course.getTeacher());
+        description.setText(course.getDescription());
+        score.setText(String.valueOf(course.getScore()));
+
+
+        return view;
+    }
+}
