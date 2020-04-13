cce218c915c2c08074661b1ad136df04761a0083
iamzzh <49836084+iamzzh@users.noreply.github.com>
2020-04-13 13:25:52 +0800

    学生认证初步ui

--- FrontEnd/app/src/main/java/com/example/uipfrontend/Admin/Fragment/AdminCertificationFragment.java
+++ FrontEnd/app/src/main/java/com/example/uipfrontend/Admin/Fragment/AdminCertificationFragment.java
@@ -4,21 +4,158 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.uipfrontend.Admin.Adapter.AdminCertificationAdapter;
+import com.example.uipfrontend.Admin.Adapter.AdminReportRecruitRecyclerViewAdapter;
 import com.example.uipfrontend.R;
+import com.jcodecraeer.xrecyclerview.ProgressStyle;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+import com.qlh.dropdownmenu.DropDownMenu;
+import com.qlh.dropdownmenu.view.MultiMenusView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class AdminCertificationFragment extends Fragment {
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//
+//        View view = inflater.inflate(R.layout.fragment_admin_certification,null);
+//        return view;
+//    }
+    private String[] headers;//菜单头部选项
+    private List<View> popupViews = new ArrayList<>();//菜单列表视图
+    private DropDownMenu dropDownMenu;
+    private MultiMenusView multiMenusView;//多级菜单
+    private XRecyclerView recyclerView;  //下拉刷新上拉加载
+    private AdminCertificationAdapter admincertificationAdapter;     //每条组队信息内容适配器
+
+    private List<String> list;   //组队信息实体类数组
+    private View rootView;  //根视图（下拉筛选框）
+    private View rootContentView;    //根视图内容
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_admin_certification,null);
-        return view;
+        if (rootView != null) {
+            ViewGroup parent = (ViewGroup) rootView.getParent();
+            if (parent != null) {
+                parent.removeView(rootView);
+            }
+        } else {
+            rootView = inflater.inflate(R.layout.fragment_student_recruit, null);
+//            recyclerView = rootView.findViewById(R.id.rv_student_group);
+            rootContentView = inflater.inflate(R.layout.fragment_admin_recruit_content,null);
+            //     rootContentView=rootView;
+            init();
+        }
+        return rootView;
+    }
+
+    private void init() {
+        //初始化下拉筛选
+        initMenus();
+        initListener();
+
+        //获取列表数据
+        getData();
+        //初始化列表
+        initRecyclerView();
+
+
+    }
+
+
+    private void initMenus() {
+
+        dropDownMenu = rootView.findViewById(R.id.dropDownMenu_student_group);
+
+        headers = new String[]{"所属院校"};
+        //初始化多级菜单
+        final String[] levelOneMenu = {"华南师范大学"};
+        final String[][] levelTwoMenu = {
+                {"计算机学院","软件学院","信息光电子学院","数学科学学院","地理科学学院","生命科学学院","外国语言文化学院","经济与管理学院","化学学院",
+                        "心理学院","文学院","法学院","物理与电信工程学院","马克思学院","历史文化学院","音乐学院","教育信息技术学院","教育科学学院"},
+                {"计算机学院","软件学院","信息光电子学院","数学科学学院","地理科学学院","生命科学学院","外国语言文化学院","经济与管理学院","化学学院",
+                        "心理学院","文学院","法学院","物理与电信工程学院","马克思学院","历史文化学院","音乐学院","教育信息技术学院","教育科学学院"},
+                {"计算机学院","软件学院","信息光电子学院","数学科学学院","地理科学学院","生命科学学院","外国语言文化学院","经济与管理学院","化学学院",
+                        "心理学院","文学院","法学院","物理与电信工程学院","马克思学院","历史文化学院","音乐学院","教育信息技术学院","教育科学学院"},
+                {"计算机学院","软件学院","信息光电子学院","数学科学学院","地理科学学院","生命科学学院","外国语言文化学院","经济与管理学院","化学学院",
+                        "心理学院","文学院","法学院","物理与电信工程学院","马克思学院","历史文化学院","音乐学院","教育信息技术学院","教育科学学院"}
+        };
+        multiMenusView = new MultiMenusView(this.getContext(),levelOneMenu,levelTwoMenu);
+        popupViews.add(multiMenusView);
+        //初始化内容视图
+        RelativeLayout contentView = rootContentView.findViewById(R.id.rl_student_recruit_recruit);
+        //装载
+        dropDownMenu.setDropDownMenu(Arrays.asList(headers),popupViews,contentView);
+
+    }
+
+    private void initListener() {
+
+        //下拉菜单
+        multiMenusView.setOnSelectListener(new MultiMenusView.OnSelectListener() {
+            @Override
+            public void getValue(String showText) {
+                dropDownMenu.setTabText(showText);
+                dropDownMenu.closeMenu();
+            }
+        });
+
+    }
+
+    private void getData(){
+        list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+    }
+
+    private void initRecyclerView() {
+
+        recyclerView = rootContentView.findViewById(R.id.rv_student_group);
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        admincertificationAdapter = new AdminCertificationAdapter(this.getContext(), list);
+        recyclerView.setAdapter(admincertificationAdapter);
+
+        recyclerView.setArrowImageView(R.drawable.iconfont_downgrey);
+        recyclerView.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
+        recyclerView.getDefaultRefreshHeaderView().setRefreshTimeVisible(true);
+        recyclerView.setLoadingMoreProgressStyle(ProgressStyle.BallRotate);
+
+        recyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                recyclerView.refreshComplete();
+
+            }
+
+            @Override
+            public void onLoadMore() {
+                recyclerView.setNoMore(true);
+            }
+        });
+
     }
 }
