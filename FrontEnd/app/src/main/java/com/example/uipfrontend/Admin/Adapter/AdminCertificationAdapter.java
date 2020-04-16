8782d26be8dafe5a89dce089e298ba37089fd934
iamzzh <49836084+iamzzh@users.noreply.github.com>
2020-04-16 16:03:23 +0800

    admin审核，学生证点击放大

--- FrontEnd/app/src/main/java/com/example/uipfrontend/Admin/Adapter/AdminCertificationAdapter.java
+++ FrontEnd/app/src/main/java/com/example/uipfrontend/Admin/Adapter/AdminCertificationAdapter.java
@@ -1,10 +1,16 @@
 package com.example.uipfrontend.Admin.Adapter;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,9 +20,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.uipfrontend.R;
 
-import com.lzy.widget.CircleImageView;
-
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.List;
 
 import info.hoang8f.widget.FButton;
@@ -45,7 +49,6 @@ public class AdminCertificationAdapter extends RecyclerView.Adapter {
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
-
             this.studentName = itemView.findViewById(R.id.admin_student_name);
             this.studentNum = itemView.findViewById(R.id.admin_student_num);
             this.portrait = itemView.findViewById(R.id.admin_student_picture);
@@ -57,28 +60,63 @@ public class AdminCertificationAdapter extends RecyclerView.Adapter {
         }
 
 
+
+
     }
+
+
 
 //
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_admin_cer, null);
+
         return new AdminCertificationAdapter.ViewHolder(view);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int pos) {
 
         AdminCertificationAdapter.ViewHolder viewHolder = new AdminCertificationAdapter.ViewHolder(holder.itemView);
 
+        viewHolder.portrait.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(context, R.style.Theme_Design_NoActionBar);
+                ImageView imgView = getView(viewHolder.portrait);
+//                ImageView imgView = viewHolder.portrait;
+                dialog.setContentView(imgView);
+                dialog.show();
+            //    dialog.dismiss();
+                imgView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+// TODO Auto-generated method stub
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
         viewHolder.studentName.setText("王大明");
         viewHolder.studentNum.setText("20172131154");
         viewHolder.studentTime.setText("2020-04-13 15:30");
         viewHolder.studentSchool.setText("华南师范大学");
         viewHolder.studentCollege.setText("计算机学院");
+
         //临时图片地址
-        setImage(context,viewHolder.portrait,"http://5b0988e595225.cdn.sohucs.com/images/20181204/bb053972948e4279b6a5c0eae3dc167e.jpeg");
+       setImage(context,viewHolder.portrait,"http://5b0988e595225.cdn.sohucs.com/images/20181204/bb053972948e4279b6a5c0eae3dc167e.jpeg");
+//        ArrayList<ImageInfo> imageInfo = new ArrayList<>();
+//        ImageInfo info = new ImageInfo();
+//        info.setThumbnailUrl("http://image.biaobaiju.com/uploads/20180111/00/1515601824-VFslzoEJkU.jpg");
+//        info.setBigImageUrl("http://image.biaobaiju.com/uploads/20180111/00/1515601824-VFslzoEJkU.jpg");
+//        imageInfo.add(info);
+//        viewHolder.portrait.setAdapter(new NineGridViewClickAdapter(context, imageInfo));
+
+
 
     }
 
@@ -99,4 +137,16 @@ public class AdminCertificationAdapter extends RecyclerView.Adapter {
         return 10;
     }
 
+    public ImageView getView(ImageView imgg) {
+        ImageView imgView = new ImageView(context);
+        imgView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        //@SuppressLint("ResourceType") InputStream is = context.getResources().openRawResource(R.drawable.portrait_default);
+//        @SuppressLint("ResourceType") InputStream is = context.getResources().openRawResource(R.drawable.portrait_default);
+//        Drawable drawable = BitmapDrawable.createFromStream(is, null);
+        Drawable drawable = imgg.getDrawable();
+        imgView.setImageDrawable(drawable);
+        return imgView;
+    }
+
 }
+
