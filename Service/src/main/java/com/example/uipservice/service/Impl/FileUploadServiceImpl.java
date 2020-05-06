f11b9c94aec9c2fb06ca45434fa6310394f59e16
Wu-peihua <13128684834@163.com>
2020-05-06 12:15:47 +0800

    多图片上传后端修改

--- Service/src/main/java/com/example/uipservice/service/Impl/FileUploadServiceImpl.java
+++ Service/src/main/java/com/example/uipservice/service/Impl/FileUploadServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -26,31 +28,38 @@ public class FileUploadServiceImpl implements FileUploadService {
     private String basePath;
 
     @Override
-    public Map upload(MultipartFile file) {
+    public Map upload(MultipartFile file[]) {
         Map modelMap = new HashMap();
-        if(file == null || file.isEmpty()){
+        if(file == null || file.length == 0){
             modelMap.put("code", 400);
             modelMap.put("msg", "未选择文件");
             return  modelMap;
         }else{
+            //以list形式返回url
+            List<String> list = new ArrayList<>();
 
-            String newFilename = UUIDUtils.getUUID() + ".png";
-            System.out.println("newFilename: " + newFilename);
+            for(MultipartFile tempFile : file){
 
-            try {
-                // 这里使用Apache的FileUtils方法来进行保存
-                FileUtils.copyInputStreamToFile(file.getInputStream(), new File(realPath, newFilename));
-                modelMap.put("code", 200);
-                modelMap.put("msg", "上传成功");
-                modelMap.put("filename", newFilename);
-                modelMap.put("url", basePath + "/images/" + newFilename);
-                System.out.println("上传文件的url：" + basePath + "/UIPImages/" + newFilename);
-            } catch (IOException e) {
-                modelMap.put("code", 500);
-                System.out.println("文件上传失败");
-                modelMap.put("msg", "文件上传失败");
-                e.printStackTrace();
+                String newFilename = UUIDUtils.getUUID() + ".png";
+
+                try {
+                    // 这里使用Apache的FileUtils方法来进行保存
+                    FileUtils.copyInputStreamToFile(tempFile.getInputStream(), new File(realPath, newFilename));
+                    list.add(basePath + "/UIPImages/" + newFilename);
+                    System.out.println("上传文件的url：" + basePath + "/UIPImages/" + newFilename);
+                } catch (IOException e) {
+                    modelMap.put("code", 500);
+                    System.out.println("文件上传失败");
+                    modelMap.put("msg", "文件上传失败");
+                    e.printStackTrace();
+                }
+
             }
+
+            modelMap.put("code", 200);
+            modelMap.put("msg", "上传成功");
+            modelMap.put("urlList",list);
+
             return modelMap;
         }
 
